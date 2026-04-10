95e5f10c5c27f299ab9e70bfa401db457e9a12a0
janse755 <tycho.jansen@kpn.com>
2026-04-10 11:48:39 +0200

    Restructure repo

--- amazon_product_recommender/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Amazon product recommender package.
-
-This package contains configuration utilities and preprocessing helpers for the
-Amazon product recommender project.
-"""
--- amazon_product_recommender/src/preprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Preprocessing helpers for Amazon product recommender.
-
-This package exposes data loading and cleaning components used by the recommender
-pipeline.
-"""
